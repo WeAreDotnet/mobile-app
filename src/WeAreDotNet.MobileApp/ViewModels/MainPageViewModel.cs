87ed29e5ad27fc6f1609794b1cae625a67d240c8
Gerald Versluis <gerald.versluis@microsoft.com>
2023-04-05 20:53:16 +0200

    Better design + navigating to URLS

--- src/WeAreDotNet.MobileApp/ViewModels/MainPageViewModel.cs
+++ src/WeAreDotNet.MobileApp/ViewModels/MainPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using WeAreDotNet.MobileApp.Models;
 using WeAreDotNet.MobileApp.Services;
 
@@ -11,6 +12,35 @@ public partial class MainPageViewModel : ObservableObject
 
     [ObservableProperty]
     private ObservableCollection<FeedEntry> feedItems = new();
+
+    [ObservableProperty]
+    private FeedEntry selectedFeedItem;
+
+    [RelayCommand]
+    private async Task FeedItemSelectedAsync()
+    {
+        if (string.IsNullOrWhiteSpace(SelectedFeedItem?.Url))
+        {
+            return;
+        }
+
+        BrowserLaunchOptions browserOptions = new()
+        {
+            LaunchMode = BrowserLaunchMode.SystemPreferred,
+        };
+
+        if (Application.Current.Resources.TryGetValue("Primary", out var colorvalue))
+        {
+            // TODO not hardcode to white
+            browserOptions.PreferredControlColor = Colors.White;
+            browserOptions.PreferredToolbarColor = (Color)colorvalue;
+        }
+
+        await Browser.Default.OpenAsync(SelectedFeedItem.Url,
+            browserOptions);
+
+        SelectedFeedItem = null;
+    }
 
     public MainPageViewModel(WeAreDotNetService weAreDotNetService)
     {
