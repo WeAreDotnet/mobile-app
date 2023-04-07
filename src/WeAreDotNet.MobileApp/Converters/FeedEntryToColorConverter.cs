7d956929e51fb1a47924f4188912b2408c2cddc6
Gerald Versluis <gerald@verslu.is>
2023-04-07 20:41:24 +0200

    Add FeedType colors to chips

---
+++ src/WeAreDotNet.MobileApp/Converters/FeedEntryToColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using WeAreDotNet.MobileApp.Models;
+
+namespace WeAreDotNet.MobileApp.Converters;
+
+public class FeedEntryToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var feedEntryType = value as string;
+
+        if (string.IsNullOrWhiteSpace(feedEntryType))
+        {
+            // TODO What is the gracefall fallback color?
+            return Colors.White;
+        }
+
+        return feedEntryType.ToLowerInvariant() switch
+        {
+            "blog" => Colors.Blue,
+            "video" => Colors.Red,
+            "sponsored" => Colors.Gray,
+            // TODO same fallback color as above
+            _ => Colors.White
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // Intentionally not implemented
+        throw new NotImplementedException();
+    }
+}
