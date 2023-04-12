313f6f0ff9a5b3b63d576492197c3204f6733468
Gerald Versluis <gerald.versluis@microsoft.com>
2023-04-12 19:31:21 +0200

    Added country flags to profile

--- src/WeAreDotNet.MobileApp/Models/AdditionalData.cs
+++ src/WeAreDotNet.MobileApp/Models/AdditionalData.cs
@@ -7,10 +7,14 @@ public class AdditionalData
     [JsonPropertyName("ghstar")]
     public Uri GitHubStarUrl { get; set; }
 
-    public bool IsGitHubStar => GitHubStarUrl is not null;
+    public bool IsGitHubStar =>
+        string.IsNullOrWhiteSpace(GitHubStarUrl?.ToString());
 
     [JsonPropertyName("xamarin")]
     public bool IsPlanetXamarinMember { get; set; }
+
+    [JsonPropertyName("msstaff")]
+    public bool IsMicrosoftEmployee { get; set; }
 
     [JsonPropertyName("womanofdotnet")]
     public bool IsWomanOfDotNetMember { get; set; }
