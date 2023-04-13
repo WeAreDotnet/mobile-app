5aa9d272ce8668a2af9f29bffde9af47348a272e
Gerald Versluis <gerald@verslu.is>
2023-04-13 21:00:25 +0200

    Add MCT + icons

--- src/WeAreDotNet.MobileApp/MauiProgram.cs
+++ src/WeAreDotNet.MobileApp/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using WeAreDotNet.MobileApp.Services;
 using WeAreDotNet.MobileApp.ViewModels;
 using WeAreDotNet.MobileApp.Views;
@@ -12,7 +13,8 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
