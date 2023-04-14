f73bf47e5580061c7517117c85d7ac56548470a2
Gerald Versluis <gerald.versluis@microsoft.com>
2023-04-14 17:03:02 +0200

    Add socials to profile

--- src/WeAreDotNet.MobileApp/ViewModels/ProfilePageViewModel.cs
+++ src/WeAreDotNet.MobileApp/ViewModels/ProfilePageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using WeAreDotNet.MobileApp.Models;
 using WeAreDotNet.MobileApp.Services;
 
@@ -17,5 +18,30 @@ public partial class ProfilePageViewModel : BaseViewModel
     public async Task LoadProfile(string nickname)
     {
         UserProfile = await weAreDotNetService.GetProfile(nickname);
+    }
+
+    [RelayCommand]
+    private async Task OpenSocialAsync(string socialUrl)
+    {
+        // TODO mostly duplicate with MainPage, refactor
+        if (string.IsNullOrWhiteSpace(socialUrl))
+        {
+            return;
+        }
+
+        BrowserLaunchOptions browserOptions = new()
+        {
+            LaunchMode = BrowserLaunchMode.SystemPreferred,
+        };
+
+        if (Application.Current.Resources.TryGetValue("Primary", out var colorvalue))
+        {
+            // TODO not hardcode to white
+            browserOptions.PreferredControlColor = Colors.White;
+            browserOptions.PreferredToolbarColor = (Color)colorvalue;
+        }
+
+        await Browser.Default.OpenAsync(socialUrl,
+            browserOptions);
     }
 }
